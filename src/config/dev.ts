a1341997be42ca683902ee8de9274b1f77efe2df
AnichoKraus <anichokraus3123@gmail.com>
2023-07-04 12:08:40 +0900

    update config

--- src/config/dev.ts
+++ src/config/dev.ts
@@ -35,7 +35,7 @@ export const DECIMAL = 1000000000000000000
 // export const CHAINID = '0x13881' //Mumbai
 export const CHAINID = '0x89' //polygon
 
-export const WALLET_STATUS_LOCALSTORAGE = ''
-export const WALLET_ADRESS_LOCALSTORAGE = ''
+export const WALLET_STATUS_LOCALSTORAGE = 'wallet'
+export const WALLET_ADRESS_LOCALSTORAGE = 'wallet_address'
 export const DEFAUL_NONE_WINNER = '0x0000000000000000000000000000000000000000'
 export const SIGN_KEY = 'VERIFY WALLET';
